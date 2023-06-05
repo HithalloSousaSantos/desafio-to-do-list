841eaa9c574259dac6f1e60af7f6eae5075401cc
Hithallo Sousa <hithallo.santos@decisaosistemas.com.br>
2023-06-05 15:07:48 -0300

    del Front

--- backend/src/application/controllers/listarAtvConcluida.ts
+++ backend/src/application/controllers/listarAtvConcluida.ts
@@ -1,7 +1,20 @@
 import { Request, Response } from 'express';
 import { listarAtividadesConcluidas } from '../../core/AtividadesConfigs';
 
+
+/**
+ * 
+ * @param request 
+ * @param response 
+ * @returns Promise<void>
+ */
+
 export const indexConcluidas = async (request: Request, response: Response): Promise<void> => {
-    const atividades = await listarAtividadesConcluidas();
-    response.render('atividadesConcluidas', { atividades });
+    try {
+        const atividades = await listarAtividadesConcluidas();
+        response.render('atividadesConcluidas', { atividades });
+    } catch (erro) {
+        console.error('Erro ao listar as atividades concluidas: ', erro);
+        response.status(500).send('Erro ao listar atividades concluídas');
+    };
 };
