302dae88c1101f503ebe6f7f754590aefd1698f5
Hithallo Sousa <hithallo.santos@decisaosistemas.com.br>
2023-06-05 15:07:31 -0300

    Del frontend

--- backend/src/application/controllers/homeController.ts
+++ backend/src/application/controllers/homeController.ts
@@ -1,11 +1,7 @@
 import {Request, Response } from 'express';
-import { listarAtividades } from '../../core/AtividadesConfigs.js';
-// import atividade from '../../core/AtividadesConfigs.js';
+import { listarAtividades } from '../../core/AtividadesConfigs';
 
 export const index = async (request: Request, response: Response): Promise<void> => {
     const atividades = await listarAtividades();
     response.render('index', { atividades }) 
 }
-
-
-//
